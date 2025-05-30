38b4187a1fb89d039cf2f92457671fab56381a6f
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-30 20:02:31 +0800

    get_next_line wip

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 12:08:26 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/30 13:32:04 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/30 15:06:02 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,12 @@
 # ifndef BUFFER_SIZE
 # define BUFFER_SIZE 100
 # endif
+
+typedef struct s_list
+{
+	char			*line;
+	int				fd;
+	struct s_list	*next;
+}					t_list;
 
 #endif
