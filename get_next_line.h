1b3e8c9d80ddb2acf8332d568b0e05dea4c96c29
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-30 13:29:17 +0800

    added stdlib and unistd in get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,14 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 12:08:26 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/30 12:09:48 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/30 13:28:50 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
 
 #endif
