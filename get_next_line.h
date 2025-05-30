acce9a1f5c03cb634882bea903b5b90a7c478d5b
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-30 13:32:43 +0800

    added BUFFER_SIZE in get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,18 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 12:08:26 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/30 13:28:50 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/30 13:32:04 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 100
+# endif
 
 #endif
