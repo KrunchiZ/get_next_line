28f3c8d876939f4612187e0a9a50798afeca364e
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-06-04 12:27:33 +0800

    moved helper functions into utils in v02

--- v02/get_next_line.c
+++ v02/get_next_line.c
@@ -6,84 +6,13 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 12:07:50 by kchiang           #+#    #+#             */
-/*   Updated: 2025/06/04 00:06:01 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/06/04 12:26:22 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* Join host and buffer together into a new malloc string and return it.
- * Host will be freed after parsing to new_host.
- */
-static char	*join_host_buffer(char *host, char *buffer, int rbytes)
-{
-	int		i;
-	int		j;
-	char	*new_host;
-
-	i = 0;
-	while (host[i])
-		i++;
-	new_host = malloc(sizeof(char) * (i + rbytes + 1));
-	if (!new_host)
-		return (NULL);
-	j = i;
-	while (j-- > 0)
-		new_host[j] = host[j];
-	j = 0;
-	while (j < rbytes && buffer[j] != '\n')
-		new_host[i++] = buffer[j++];
-	if (j < rbytes)
-		new_host[i++] = buffer[j++];
-	new_host[i] = '\0';
-	i = 0;
-	while (j < rbytes)
-		buffer[i++] = buffer[j++];
-	buffer[i] = '\0';
-	return (free(host), new_host);
-}
-
-/* Read fd and combine host(if there's any from previous g_n_line call) with
- * buffer until end of first line into new_host.
- * If host is NULL, malloc an empty string into host.
- *
- * Leftover characters gets shifted to the front and stays in buffer to be
- * processed by extract_buffer or string_transfer in the next g_n_line call.
- *
- * When end of file has been reached (0 rbytes), free host(empty string or NULL)
- * then return NULL.
- *
- * If incomplete line is found after read, line will be transferred from
- * new_host back to a freed buffer.
- */
-static char	*read_fd(char *host, char *buffer, int fd)
-{
-	int		rbytes;
-	char	*new_host;
-
-	rbytes = read(fd, buffer, BUFFER_SIZE);
-	if (rbytes <= 0)
-		return (host);
-	if (!host)
-	{
-		host = malloc(sizeof(char) * 1);
-		if (!host)
-			return (NULL);
-		host[0] = '\0';
-	}
-	new_host = join_host_buffer(host, buffer, rbytes);
-	if (!new_host)
-		return (NULL);
-	if (!has_newline(new_host))
-	{
-		host = string_transfer(new_host);
-		free(new_host);
-		new_host = read_fd(host, buffer, fd);
-	}
-	return (new_host);
-}
-
-/* Buffer has to be null terminated to differentiate between true complete
+/* Buffer is null terminated to differentiate between true complete
  * line and unfinished line.
  * 
  * Non \n terminated buffer will be freed in 'buffer_to_host' after getting
--- v02/get_next_line_utils.c
+++ v02/get_next_line_utils.c
@@ -6,15 +6,11 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 12:12:43 by kchiang           #+#    #+#             */
-/*   Updated: 2025/06/04 00:06:01 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/06/04 12:26:19 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-/* ************************************************************************** */
-/* FUNCTIONS FOR BUFFER CHECKING WHEN GET_NEXT_LINE GETS RECALLED             */
-/* ************************************************************************** */
 
 /* Check to see if previous buffer has a complete line
  */
@@ -86,4 +82,75 @@ char	*string_transfer(char *src)
 	}
 	dest[i] = '\0';
 	return (dest);
+}
+
+/* Join host and buffer together into a new malloc string and return it.
+ * Host will be freed after parsing to new_host.
+ */
+char	*join_host_buffer(char *host, char *buffer, int rbytes)
+{
+	int		i;
+	int		j;
+	char	*new_host;
+
+	i = 0;
+	while (host[i])
+		i++;
+	new_host = malloc(sizeof(char) * (i + rbytes + 1));
+	if (!new_host)
+		return (NULL);
+	j = i;
+	while (j-- > 0)
+		new_host[j] = host[j];
+	j = 0;
+	while (j < rbytes && buffer[j] != '\n')
+		new_host[i++] = buffer[j++];
+	if (j < rbytes)
+		new_host[i++] = buffer[j++];
+	new_host[i] = '\0';
+	i = 0;
+	while (j < rbytes)
+		buffer[i++] = buffer[j++];
+	buffer[i] = '\0';
+	return (free(host), new_host);
+}
+
+/* Read fd and combine host(if there's any from previous g_n_line call) with
+ * buffer until end of first line into new_host.
+ * If host is NULL, malloc an empty string into host.
+ *
+ * Leftover characters gets shifted to the front and stays in buffer to be
+ * processed by extract_buffer or string_transfer in the next g_n_line call.
+ *
+ * When end of file has been reached (0 rbytes), free host(empty string or NULL)
+ * then return NULL.
+ *
+ * If incomplete line is found after read, line will be transferred from
+ * new_host back to a freed buffer.
+ */
+char	*read_fd(char *host, char *buffer, int fd)
+{
+	int		rbytes;
+	char	*new_host;
+
+	rbytes = read(fd, buffer, BUFFER_SIZE);
+	if (rbytes <= 0)
+		return (host);
+	if (!host)
+	{
+		host = malloc(sizeof(char) * 1);
+		if (!host)
+			return (NULL);
+		host[0] = '\0';
+	}
+	new_host = join_host_buffer(host, buffer, rbytes);
+	if (!new_host)
+		return (NULL);
+	if (!has_newline(new_host))
+	{
+		host = string_transfer(new_host);
+		free(new_host);
+		new_host = read_fd(host, buffer, fd);
+	}
+	return (new_host);
 }
