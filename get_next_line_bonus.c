d00218b66536b3a7d89fea5c6f23c40781c32f05
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-06-04 13:05:26 +0800

    added bonus for the malloc version

---
+++ get_next_line_bonus.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 12:07:50 by kchiang           #+#    #+#             */
+/*   Updated: 2025/06/04 13:04:52 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+/* Buffer is null terminated to differentiate between true complete
+ * line and unfinished line.
+ * 
+ * Non \n terminated buffer will be freed in 'string_transfer' after getting
+ * parsed to host.
+ */
+char	*get_next_line(int fd)
+{
+	char		*host;
+	static char	*buffer[MAX_FDS];
+
+	if (fd < 0)
+		return (NULL);
+	host = NULL;
+	if (buffer[fd])
+	{
+		if (has_newline(buffer[fd]))
+			return (extract_buffer(buffer[fd]));
+		host = string_transfer(buffer[fd]);
+	}
+	buffer[fd] = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer[fd])
+		return (free(host), NULL);
+	buffer[fd][0] = '\0';
+	host = read_fd(host, buffer[fd], fd);
+	if (host)
+		return (host);
+	free(buffer[fd]);
+	buffer[fd] = NULL;
+	return (NULL);
+}
